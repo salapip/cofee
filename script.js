fd66e53ef5e9672457bda422240d011cabe5cdc9
salapip <apipdedes51@gmail.com>
2024-11-20 11:17:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+//let
+let namaDepan = "Muhammad";
+console.log(namaDepan);
+
+let umur = 17
+console.log(umur)
+
+//scooping let
+{
+  let namaDepan = "bakwan";
+  console.log(namaDepan);
+}
+
+//let ketika diganti hanya mengambil nilainya saja
+namaDepan = "Salapip";
+console.log(namaDepan);
+
+//var
+var namaBelakang = "al-Falah";
+console.log(namaBelakang);
+
+var umurSaya = 18;
+console.log(umurSaya);
+
+{
+  var namaBelakang = "teh jus";
+  console.log(namaBelakang);
+}
+
+// var ketika diganti sama dengan let: hanya mengambil nilainya saja
+namaBelakang = "salapip";
+console.log(namaBelakang);
+
+//const
+const namaSaya = "Muhammad Apip";
+console.log(namaSaya);
+
+const umurKami = 20;
+console.log(umurKami);
+
+// const tidak bisa diganti karena konstan, ketika diganti akan terjadi error
+namaSaya = "Apip";
+console.log(namaSaya);
+
+umurKami = 25;
+console.log(umurKami);
